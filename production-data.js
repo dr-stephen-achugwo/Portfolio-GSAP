70809519cac1ce1090c245418b6f4434c4b149a2
Stephen Achugwo <dr_stephen@aol.com>
2023-05-09 13:39:58 +0100

    Add files via upload

---
+++ production-data.js
@@ -0,0 +1,382 @@
+export const allProductions = [
+	{
+		title: "Coca Cola Intergalactic",
+		img: "/images/productions/intergalactic.jpg",
+		url: "https://vimeo.com/728655671",
+	},
+	{
+		title: "Xiaomi 11T Pro Parkour Video",
+		img: "/images/productions/11-t-pro-parkour.jpg",
+		url: "https://vimeo.com/728655763",
+	},
+	{
+		title: "Xiaomi - Sınırlarını Zorla",
+		img: "/images/productions/xiaomi.jpg",
+		url: "https://vimeo.com/545755569",
+	},
+	{
+		title: "Taste Your Journey",
+		img: "/images/productions/taste-your-journey.jpg",
+		url: "https://vimeo.com/536106276",
+	},
+	{
+		title: "Portonovi Project",
+		img: "/images/productions/portonovi.jpg",
+		url: "https://vimeo.com/536394847",
+	},
+	{
+		title: "Edirne",
+		img: "/images/productions/edirne.jpg",
+		url: "https://vimeo.com/728655541",
+	},
+	{
+		title: "Ülker Teremyağ",
+		img: "/images/productions/teremyag.jpg",
+		url: "https://vimeo.com/728655395",
+	},
+	{
+		title: "Yalanlar Music Clip",
+		img: "/images/productions/yalanlar.jpg",
+		url: "https://vimeo.com/497971043",
+	},
+	{
+		title: "Avcı Interior",
+		img: "/images/productions/interior.jpg",
+		url: "https://vimeo.com/451153012",
+	},
+	{
+		title: "The Passion",
+		img: "/images/productions/the-passion.jpg",
+		url: "https://vimeo.com/399683774",
+	},
+	{
+		title: "Slow Down",
+		img: "/images/productions/slow-down.jpg",
+		url: "https://vimeo.com/389929131",
+	},
+	{
+		title: "#artwithus | agt",
+		img: "/images/productions/art-with-us.jpg",
+		url: "https://vimeo.com/389923180",
+	},
+	{
+		title: "Showreel",
+		img: "/images/productions/showreel.jpg",
+		url: "https://vimeo.com/381624477",
+	},
+	{
+		title: "Lunar Circus Promo Film",
+		img: "/images/productions/lunar.jpg",
+		url: "https://vimeo.com/374399400",
+	},
+	{
+		title: "SPX - How Big",
+		img: "/images/productions/spx.jpg",
+		url: "https://vimeo.com/374397445",
+	},
+	{
+		title: "TANDEM DANCE THEATRE PROMO FILM",
+		img: "/images/productions/tandem-promo.jpg",
+		url: "https://vimeo.com/365480996",
+	},
+	{
+		title: "NORTHMEN 1. YIL FILM",
+		img: "/images/productions/northmen.jpg",
+		url: "https://vimeo.com/363542216",
+	},
+	{
+		title: "TANDEM DANCE THEATRE TEASER",
+		img: "/images/productions/tandem-promo-teaser.jpg",
+		url: "https://vimeo.com/363547177",
+	},
+	{
+		title: "FINAL JAM RECAP",
+		img: "/images/productions/final-jam-recap.jpg",
+		url: "https://vimeo.com/357361066",
+	},
+	{
+		title: "KRAFT FESTIVAL",
+		img: "/images/productions/kraft-festival.jpg",
+		url: "https://vimeo.com/348397100",
+	},
+	{
+		title: "RAPID DANCE",
+		img: "/images/productions/rapid-dance.jpg",
+		url: "https://vimeo.com/347025216",
+	},
+	{
+		title: "SPA SOUL | HYATT CENTRİC LEVENT İSTANBUL",
+		img: "/images/productions/spa-soul.jpg",
+		url: "https://vimeo.com/336091625",
+	},
+	{
+		title: "RENEKA R80 COMMERCIAL",
+		img: "/images/productions/reneka.jpg",
+		url: "https://vimeo.com/323702052",
+	},
+	{
+		title: "2019 SHOWREEL",
+		img: "/images/productions/2019-reel.jpg",
+		url: "https://vimeo.com/331442093",
+	},
+	{
+		title: "CRYSTAL GELATO FILM",
+		img: "/images/productions/crystal-gelato.jpg",
+		url: "https://vimeo.com/312514219",
+	},
+	{
+		title: "THE SUDD COFFEE – CHRISTMAS FILM",
+		img: "/images/productions/christmas.jpg",
+		url: "https://vimeo.com/308987488",
+	},
+	{
+		title: "CARDI B – I LIKE IT | HİPHOP | CHOREOGRAPHY MUSIC CLIP",
+		img: "/images/productions/i-like-it-like-that.jpg",
+		url: "https://vimeo.com/371645946",
+	},
+	{
+		title: "ZAYN – FINGERS | DANCE PERFORMANCE",
+		img: "/images/productions/fingers.jpg",
+		url: "https://vimeo.com/381060937",
+	},
+	{
+		title: "APES**T DANCE CHOREOGRAPHY",
+		img: "/images/productions/apeshit.jpg",
+		url: "https://vimeo.com/301697239",
+	},
+	{
+		title: "CRYSTAL & SMACH EVENT FILM",
+		img: "/images/productions/crystal-event.jpg",
+		url: "https://vimeo.com/312514219",
+		// youtube
+	},
+	{
+		title: "AŞKIN HALI TOZ DUMAN – MUSIC CLIP",
+		img: "/images/productions/askin-hali.jpg",
+		url: "https://vimeo.com/273713790",
+	},
+	{
+		title: "DRONE THE HOTEL | CALISTA LUXURY HOTEL",
+		img: "/images/productions/drone.jpg",
+		url: "https://vimeo.com/301711984",
+	},
+	{
+		title: "TENNIS LIFE | CALISTA LUXURY HOTEL",
+		img: "/images/productions/tennis.jpg",
+		url: "https://vimeo.com/301712700",
+	},
+	{
+		title: "TIMELAPSE MOOD | CALISTA LUXURY HOTEL",
+		img: "/images/productions/timelapse.jpg",
+		url: "https://vimeo.com/301712934",
+	},
+	{
+		title: "GYMFACTORY FITNESS CLUB | HYPER SPORT",
+		img: "/images/productions/hyper-sport.jpg",
+		url: "https://vimeo.com/301718360",
+	},
+	{
+		title: "GYMFACTORY FITNESS CLUB | SPORT TİME",
+		img: "/images/productions/sport-time.jpg",
+		url: "https://vimeo.com/301718451",
+	},
+	{
+		title: "REDBULL | KICK IT",
+		img: "/images/productions/kick-it.jpg",
+		url: "https://vimeo.com/301719024",
+	},
+]
+
+export const commercialProductions = [
+	{
+		title: "Coca Cola Intergalactic",
+		img: "/images/productions/intergalactic.jpg",
+		url: "https://vimeo.com/728655671",
+	},
+	{
+		title: "Xiaomi 11T Pro Parkour Video",
+		img: "/images/productions/11-t-pro-parkour.jpg",
+		url: "https://vimeo.com/728655763",
+	},
+	{
+		title: "Xiaomi - Sınırlarını Zorla",
+		img: "/images/productions/xiaomi.jpg",
+		url: "https://vimeo.com/545755569",
+	},
+	{
+		title: "Portonovi Project",
+		img: "/images/productions/portonovi.jpg",
+		url: "https://vimeo.com/536394847",
+	},
+	{
+		title: "Taste Your Journey",
+		img: "/images/productions/taste-your-journey.jpg",
+		url: "https://vimeo.com/536106276",
+	},
+	{
+		title: "Ülker Teremyağ",
+		img: "/images/productions/teremyag.jpg",
+		url: "https://vimeo.com/728655395",
+	},
+	{
+		title: "Avcı Interior",
+		img: "/images/productions/interior.jpg",
+		url: "https://vimeo.com/451153012",
+	},
+	{
+		title: "The Passion",
+		img: "/images/productions/the-passion.jpg",
+		url: "https://vimeo.com/399683774",
+	},
+	{
+		title: "#artwithus | agt",
+		img: "/images/productions/art-with-us.jpg",
+		url: "https://vimeo.com/389923180",
+	},
+	{
+		title: "Showreel",
+		img: "/images/productions/showreel.jpg",
+		url: "https://vimeo.com/381624477",
+	},
+	{
+		title: "Lunar Circus Promo Film",
+		img: "/images/productions/lunar.jpg",
+		url: "https://vimeo.com/374399400",
+	},
+	{
+		title: "SPX - How Big",
+		img: "/images/productions/spx.jpg",
+		url: "https://vimeo.com/374397445",
+	},
+	{
+		title: "TANDEM DANCE THEATRE PROMO FILM",
+		img: "/images/productions/tandem-promo.jpg",
+		url: "https://vimeo.com/365480996",
+	},
+	{
+		title: "NORTHMEN 1. YIL FILM",
+		img: "/images/productions/northmen.jpg",
+		url: "https://vimeo.com/363542216",
+	},
+	{
+		title: "TANDEM DANCE THEATRE TEASER",
+		img: "/images/productions/tandem-promo-teaser.jpg",
+		url: "https://vimeo.com/363547177",
+	},
+	{
+		title: "FINAL JAM RECAP",
+		img: "/images/productions/final-jam-recap.jpg",
+		url: "https://vimeo.com/357361066",
+	},
+	{
+		title: "KRAFT FESTIVAL",
+		img: "/images/productions/kraft-festival.jpg",
+		url: "https://vimeo.com/348397100",
+	},
+	{
+		title: "RAPID DANCE",
+		img: "/images/productions/rapid-dance.jpg",
+		url: "https://vimeo.com/347025216",
+	},
+	{
+		title: "SPA SOUL | HYATT CENTRİC LEVENT İSTANBUL",
+		img: "/images/productions/spa-soul.jpg",
+		url: "https://vimeo.com/336091625",
+	},
+	{
+		title: "RENEKA R80 COMMERCIAL",
+		img: "/images/productions/reneka.jpg",
+		url: "https://vimeo.com/323702052",
+	},
+	{
+		title: "SHOWREEL",
+		img: "/images/productions/2019-reel.jpg",
+		url: "https://vimeo.com/331442093",
+	},
+	{
+		title: "CRYSTAL GELATO FILM",
+		img: "/images/productions/crystal-gelato.jpg",
+		url: "https://vimeo.com/312514219",
+	},
+	{
+		title: "THE SUDD COFFEE – CHRISTMAS FILM",
+		img: "/images/productions/christmas.jpg",
+		url: "https://vimeo.com/308987488",
+	},
+	{
+		title: "CRYSTAL & SMACH EVENT FIL",
+		img: "/images/productions/crystal-event.jpg",
+		url: "https://vimeo.com/312514219",
+	},
+]
+
+export const creativeProductions = [
+	{
+		title: "Edirne",
+		img: "/images/productions/edirne.jpg",
+		url: "https://vimeo.com/728655541",
+	},
+	{
+		title: "DRONE THE HOTEL | CALISTA LUXURY HOTEL",
+		img: "/images/productions/drone.jpg",
+		url: "https://vimeo.com/301711984",
+	},
+	{
+		title: "TENNIS LIFE | CALISTA LUXURY HOTEL",
+		img: "/images/productions/tennis.jpg",
+		url: "https://vimeo.com/301712700",
+	},
+	{
+		title: "TIMELAPSE MOOD | CALISTA LUXURY HOTEL",
+		img: "/images/productions/timelapse.jpg",
+		url: "https://vimeo.com/301712934",
+	},
+	{
+		title: "GYMFACTORY FITNESS CLUB | HYPER SPORT",
+		img: "/images/productions/hyper-sport.jpg",
+		url: "https://vimeo.com/301718360",
+	},
+	{
+		title: "GYMFACTORY FITNESS CLUB | SPORT TİME",
+		img: "/images/productions/sport-time.jpg",
+		url: "https://vimeo.com/301718451",
+	},
+	{
+		title: "REDBULL | KICK IT",
+		img: "/images/productions/kick-it.jpg",
+		url: "https://vimeo.com/301719024",
+	},
+]
+
+export const musicVideoProductions = [
+	{
+		title: "Yalanlar Music Clip",
+		img: "/images/productions/yalanlar.jpg",
+		url: "https://vimeo.com/497971043",
+	},
+	{
+		title: "Slow Down",
+		img: "/images/productions/slow-down.jpg",
+		url: "https://vimeo.com/389929131",
+	},
+	{
+		title: "CARDI B – I LIKE IT | HİPHOP | CHOREOGRAPHY MUSIC CLIP",
+		img: "/images/productions/i-like-it-like-that.jpg",
+		url: "https://vimeo.com/371645946",
+	},
+	{
+		title: "ZAYN – FINGERS | DANCE PERFORMANCE",
+		img: "/images/productions/fingers.jpg",
+		url: "https://vimeo.com/381060937",
+	},
+	{
+		title: "APES**T DANCE CHOREOGRAPHY",
+		img: "/images/productions/apeshit.jpg",
+		url: "https://vimeo.com/301697239",
+	},
+	{
+		title: "AŞKIN HALI TOZ DUMAN – MUSIC CLIP",
+		img: "/images/productions/askin-hali.jpg",
+		url: "https://vimeo.com/273713790",
+	},
+]
